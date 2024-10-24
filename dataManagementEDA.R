9c5fa40173ce9e64fd2f9618e49db0c7c28fd0a7
varsha1241 <varshavenkat2001@gmail.com>
2024-10-24 09:23:26 +0100

    exercise question 1

--- dataManagementEDA.R
+++ dataManagementEDA.R
@@ -1,19 +1,22 @@
 library(dplyr)
 library(nycflights13)
 data("flights")
-flights
+glimpse(flights)
 
+flights
 # 1. Had arrival delay of two or more hours
-flights |> filter(dep_delay >= 2)
+flights |> filter(dep_delay >= 120)
 
 # 2. Flew to Houston 
-flights |> filter(dest == "IAH" | dest == "HOU")
+flights |> filter(dest %in% c("IAH" ,"HOU"))
 
 # 3. were operated by united , american or delta
-flights |> filter(carrier == "UA" | carrier == "DL" )
+flights |> filter(carrier %in% c("UA" ,"DL", "AA" ))
 
 # 4. departed in summer (July ,August and September)
-flights |> filter(month == 7 | month == 8 | month == 9)
+flights |> filter(month %n% c(7 , 8 , 9))
 
 # 5. arrived more than 2 hours late but did not leave late
-flights |> filter(arr_delay >2 & dep_delay == 0)
+flights |> filter(arr_delay >=120 & dep_delay <= 0) |> View()
+
+?flights
